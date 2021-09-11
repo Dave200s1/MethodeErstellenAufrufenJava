bdcef6ce81ab44ff78153b67e853e02f077baff5
Dave200s1 <81190030+Dave200s1@users.noreply.github.com>
2021-09-11 14:24:23 +0200

    Add files via upload

---
+++ New.java
@@ -0,0 +1,12 @@
+public class New {
+    public static void main(String[]args){
+        //Methode aufrufen
+        addExclamationMark("Hot dogs");
+    }
+
+    //Methode erstellen Aufrufezeichen
+
+    public static void addExclamationMark(String s){
+        System.out.println(s + "!");
+    }
+}
